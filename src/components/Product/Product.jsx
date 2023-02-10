7333352c59935c9ac6a197936fa66f4a164447d2
kieronthomas <kieron.thomas321@hotmail.co.uk>
2023-02-10 14:02:17 +0000

    first commit

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,102 @@
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./product.css";
+import { motion } from "framer-motion";
+import { CartContext } from "../../App";
+
+const Product = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState({});
+  const { setCart } = useContext(CartContext);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+      const data = await response.json();
+      setProduct(data);
+    };
+    fetchProduct();
+
+    const storedProducts = localStorage.getItem("cart");
+    if (storedProducts) {
+      setCart(JSON.parse(storedProducts));
+    }
+  }, [id]);
+
+  const imageAnimation = {
+    initial: {
+      scale: 0,
+    },
+    animate: {
+      scale: 1,
+      transition: {
+        duration: 0.5,
+        delay: 0.5,
+        type: "spring",
+        stiffness: 300,
+        damping: 10,
+      },
+    },
+  };
+
+  const productAnimation = {
+    initial: {
+      scale: 0,
+    },
+    animate: {
+      scale: 1,
+      transition: {
+        duration: 0.5,
+        delay: 0.7,
+        type: "spring",
+        stiffness: 300,
+        damping: 10,
+      },
+    },
+  };
+
+  console.log(product);
+  return (
+    <div className="product" key={product.id}>
+      <motion.div
+        className="product_image"
+        variants={imageAnimation}
+        initial="initial"
+        animate="animate"
+      >
+        <img src={product.image} alt="" />
+      </motion.div>
+      <motion.div
+        className="product_info"
+        variants={productAnimation}
+        initial="initial"
+        animate="animate"
+      >
+        <h1>{product.title}</h1>
+        <h2>£{product.price}</h2>
+        <h3>
+          <span>Description: </span>
+          {product.description}
+        </h3>
+        <div className="quantity">
+          <p>Quantity: </p>
+          <select>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+        <button>BUY NOW</button>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Product;
